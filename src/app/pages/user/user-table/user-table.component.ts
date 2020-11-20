b717dfc719bddd7dd272684f53a0dc3ace6e87ef
AbduTarabay <abdutarabay@hotmail.com>
2020-11-20 19:56:38 -0300

    Add about/tutorial

--- src/app/pages/user/user-table/user-table.component.ts
+++ src/app/pages/user/user-table/user-table.component.ts
@@ -6,6 +6,27 @@ import { User } from 'src/app/models/User';
 import { UserService } from 'src/app/services/user.service';
 import { Notifications } from 'src/app/utils/Notifications';
 
+export interface PeriodicElement {
+  id:number;
+  name:string;
+  email:string;
+  cpf:string;
+  zipCode:string;
+  state:string;
+  city:string;
+  place:string;
+  neighborhood:string;
+  complement:string;
+  number:number; 
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {id: 1, name: 'Pedro', email:'Pedro@hotmail.com',cpf: '189.126.458-09',zipCode:'85460-000',state:'PR',city:'Quedas do Iguaçu',place:'rua tarumã',neighborhood:'vila dias',complement:'casa',number:27},
+  {id: 2, name: 'João', email:'Joao@hotmail.com',cpf: '226.979.225-15',zipCode:'04546-001',state:'SP',city:'São Paulo',place:'av Brasil',neighborhood:'Centro',complement:'ap 405',number:772},
+  {id: 3, name: 'Marcos', email:'Marcos@hotmail.com',cpf: '515.654.983-11',zipCode:'01333-010',state:'RJ',city:'Paraty',place:'av atlântico',neighborhood:'Vila Lobos',complement:'sítio',number:0},
+  
+];
+
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
@@ -31,7 +52,8 @@ export class UserTableComponent extends Notifications implements OnInit {
     'number', 
     'actions'
   ];
-  public dataSource:MatTableDataSource<User> = new MatTableDataSource();
+  public dataSource:MatTableDataSource<any> = new MatTableDataSource(ELEMENT_DATA);
+
 
   constructor(private router:Router,private userService: UserService, public toastrService: ToastService) {
     super(toastrService);
